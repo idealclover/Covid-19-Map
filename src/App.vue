2bbf87388ece2ddaa6ce011b1cfbb770bfb7954c
idealclover <idealclover@live.com>
2022-06-04 21:37:38 +0800

    :tada: update url

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@
     <div id="cnmap" class="map"></div>
     <div id="worldmap" class="map"></div>
     <div class="copyright">
-      <a target="_blank" href="https://idealclover.top"
+      <a target="_blank" href="https://idealclover.top/archives/634/"
         >调用地图</a
       >
     </div>
